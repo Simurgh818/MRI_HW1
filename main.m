dfb4d6b08c1f61df4c6a209589b5a3e57fdd19b7
Sina Dabiri <sina.dabiri@berkeley.edu>
2022-02-13 23:59:01 -0500

    draft of 1c

--- main.m
+++ main.m
@@ -15,16 +15,28 @@
 w_rf = 63.835e6; 
 delta_w_rf = 2550;
 alpha = 90; 
-T = 0.5;
-time = [0:0.01:T];
-B1 = zeros(1,length(time));
+gamma = 42.5e6;
+T = [0.5, 0.1, 1.5];
+for p=1: length(T)
 
-for t=1:length(time)
-
-    Gauss = exp(-(time(t)-w_rf)^2/(2*delta_w_rf^2));
-    B1(t) = Gauss * (cos(w_rf*time(t)+alpha)*1i +...
-        sin(w_rf*time(t) +alpha)*1j);
+    time = [0:0.01:T(p)];
+    Gauss = zeros(1, length(time));
+    B1 = zeros(1,length(time));
+    
+    for t=1:length(time)
+        B1(t) = alpha/(gamma*time(t));
+    %     Gauss(t) = exp(-(time(t)-w_rf)^2/(2*(delta_w_rf)^2));
+    %     B1(t) = Gauss(t)* (cos(w_rf*time(t)+alpha)*1i +...
+    %         sin(w_rf*time(t) +alpha)*1j);
+    
+    end
+    subplot(3,1,p)
+    plot(time, B1)
+    xlabel('msec')
+    ylabel('Tesla')
 
 end
+f =  exp(-(t-w_rf)^2/(2*(delta_w_rf)^2))*(cos(w_rf*t+alpha)*1i + sin(w_rf*t +alpha)*1j);
 
-plot(time, B1)
+% cycles = gamma* int(f,t,0, 1e-3);
+% fprintf('The precession cycles in lab frame is %1.3g ', cycles);
